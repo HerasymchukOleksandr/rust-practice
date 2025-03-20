574db483004ee724ce1d6c8af6436a0ad4476e1a
HerasymchukOleksandr <sasha.gerasimchyk05@gmail.com>
2025-03-20 22:10:29 +0200

    Практична робота №3

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -1,4 +1,19 @@
 #[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+fn envelope (){
+    const W: u32 =30;
+    const H: u32 = 15;
+
+    for y in 0..H {
+        for x in 0..W {
+            let is_hor = y ==0 || y == H-1;
+            let is_ver = x ==0 || x == W-1;
+            let is_diag =x==y*2;
+            let is_diag2 = y*2==W-1-x;
+            let is_show = is_hor || is_ver || is_diag || is_diag2;
+
+            let sym = if is_show {'*'} else {' '};
+            print!("{}", sym);
+        }
+        println!();
+    }
 }
